f0dbb9b8e473dccb50f859de5397529aba737617
goku-m <elivation17@gmail.com>
2026-08-17 23:26:22 -0700

    prefinance log pause

--- internal/pages/logs.go
+++ internal/pages/logs.go
@@ -311,8 +311,7 @@ func Logs() templ.Component {
           const communitiesText = communityNames ? toPascalText(communityNames) : "N/A";
           const dateTime = "<span class=\"log-datetime\">" + log.date + ": " + log.time + "</span>";
           if (log.action === "updated") {
-            const amount = Number(log.amount || 0).toLocaleString(undefined, { maximumFractionDigits: 2 });
-            item.innerHTML = dateTime + " <span class=\"log-strong\">" + toPascalText(log.updatedBy) + "</span> from <span class=\"log-strong\">" + toPascalText(log.zoneName) + "</span> issued <span class=\"log-strong\">GHc</span> <span class=\"log-strong\">" + amount + "</span> in <span class=\"log-strong\">Pre-Finance</span> in communities: <span class=\"log-strong\">" + communitiesText + "</span>.";
+            item.innerHTML = dateTime + " <span class=\"log-strong\">" + toPascalText(log.updatedBy) + "</span> from <span class=\"log-strong\">" + toPascalText(log.zoneName) + "</span> updated records in communities: <span class=\"log-strong\">" + communitiesText + "</span>.";
           } else if (log.action === "weighed") {
             const weight = Number(log.weightKg || 0).toLocaleString(undefined, { maximumFractionDigits: 2 });
             const amount = Number(log.amount || 0).toLocaleString(undefined, { maximumFractionDigits: 2 });
